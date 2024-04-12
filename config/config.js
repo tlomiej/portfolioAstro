b5488238663668ab5061dc7a5ea0291df60d9986
tlomiej <bartekstaron@gmail.com>
2024-04-12 23:41:06 +0200

    addproject descryption

--- config/config.js
+++ config/config.js
@@ -1,5 +1,21 @@
-export const outerProfilLinks =[
-    {url: 'https://www.linkedin.com/in/bart%C5%82omiej-staro%C5%84', name: "Linkedin",icon: "uil:linkedin"},
-    {url: 'https://github.com/tlomiej', name: "Github", icon:"uil:github"},
+export const outerProfilLinks = [
+  {
+    url: "https://www.linkedin.com/in/bart%C5%82omiej-staro%C5%84",
+    name: "Linkedin",
+    icon: "uil:linkedin",
+  },
+  { url: "https://github.com/tlomiej", name: "Github", icon: "uil:github" },
   /*   {url: 'https://github.com/tlomiej', name: "Github", icon:"mongodb"} */
-]
+];
+
+export const projects = [{
+  name: "BrawlStarAPI",
+  tools: [
+    {
+      url: "https://www.linkedin.com/in/bart%C5%82omiej-staro%C5%84",
+      name: "Linkedin",
+      icon: "uil:linkedin",
+    },
+    { url: "https://github.com/tlomiej", name: "Github", icon: "uil:github" },
+  ],
+}];
