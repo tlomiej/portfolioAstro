576c2ecd292431bb5b4b66d6c031c869d38e58bf
tlomiej <bartekstaron@gmail.com>
2024-04-12 13:36:52 +0200

    react

--- src/components/react/LinkComponent.tsx
+++ src/components/react/LinkComponent.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, type JSXElementConstructor, type ReactElement, type ReactNode, type ReactPortal } from "react";
 
-export default function Welcome(props) {
+export default function Welcome(props: { name?: string }) {
   const [count, setCount] = useState(0);
 
   return (
