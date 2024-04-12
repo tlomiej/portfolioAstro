34c422039df15475e405ab782c58ca8a13c94bf8
tlomiej <bartekstaron@gmail.com>
2024-04-12 15:53:35 +0200

    icons

---
+++ config/config.js
@@ -0,0 +1,5 @@
+export const outerProfilLinks =[
+    {url: 'https://www.linkedin.com/in/bart%C5%82omiej-staro%C5%84', name: "Linkedin",icon: "uil:linkedin"},
+    {url: 'https://github.com/tlomiej', name: "Github", icon:"uil:github"},
+  /*   {url: 'https://github.com/tlomiej', name: "Github", icon:"mongodb"} */
+]
