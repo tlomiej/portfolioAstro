b5352b90cc0b72d205106f422bbd6c4743005da8
tlomiej <bartekstaron@gmail.com>
2024-04-13 16:00:23 +0200

    config

--- config/config.js
+++ config/config.js
@@ -8,14 +8,30 @@ export const outerProfilLinks = [
   /*   {url: 'https://github.com/tlomiej', name: "Github", icon:"mongodb"} */
 ];
 
-export const projects = [{
-  name: "BrawlStarAPI",
-  tools: [
-    {
-      url: "https://www.linkedin.com/in/bart%C5%82omiej-staro%C5%84",
-      name: "Linkedin",
-      icon: "uil:linkedin",
-    },
-    { url: "https://github.com/tlomiej", name: "Github", icon: "uil:github" },
-  ],
-}];
+export const projects = [
+  {
+    name: "BrawlStarAPI",
+    tools: [
+      {
+        url: "https://www.linkedin.com/in/bart%C5%82omiej-staro%C5%84",
+        name: "Linkedin",
+        icon: "uil:linkedin",
+      },
+      { url: "https://github.com/tlomiej", name: "Github", icon: "uil:github" },
+    ],
+  },
+];
+
+const projectsbyID = {
+  BrawlStarAPI: {
+    name: "BrawlStarAPI",
+    tools: [
+      {
+        url: "https://www.linkedin.com/in/bart%C5%82omiej-staro%C5%84",
+        name: "Linkedin",
+        icon: "uil:linkedin",
+      },
+      { url: "https://github.com/tlomiej", name: "Github", icon: "uil:github" },
+    ],
+  },
+};
