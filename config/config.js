b4c06e4d23b38e3b71c88cd247c13c0745e1b2a5
tlomiej <bartekstaron@gmail.com>
2024-04-14 08:10:58 +0200

    dynamic pages for projects

--- config/config.js
+++ config/config.js
@@ -23,13 +23,56 @@ export const projects = [
 ];
 
 export const projectsbyID = {
-  BrawlStarAPI: {
+  brawlstarapi: {
     name: "BrawlStarAPI",
+    descryption: "BrawlStarAPI",
     tools: [
       {
         url: "https://nodejs.org",
         name: "node.js",
         icon: "nodejs",
+      },
+      {
+        url: "https://expressjs.com",
+        name: "express.js",
+        icon: "express",
+      },
+      {
+        url: "https://ejs.co/",
+        name: "EJS",
+        icon: "ejs",
+      },
+      {
+        url: "https://www.mongodb.com",
+        name: "mongoDB",
+        icon: "mongodb",
+      },
+      { url: "https://github.com/tlomiej", name: "Github", icon: "uil:github" },
+    ],
+  },
+  geosearch: {
+    name: "GeoSearch",
+    descryption: "GeoSearch - looking for addres",
+    tools: [
+      {
+        url: "https://nodejs.org",
+        name: "node.js",
+        icon: "nodejs",
+      },
+      {
+        url: "https://expressjs.com",
+        name: "express.js",
+        icon: "express",
+      },
+      {
+        url: "https://ejs.co/",
+        name: "EJS",
+        icon: "ejs",
+      },
+      {
+        url: "https://www.mongodb.com",
+        name: "mongoDB",
+        icon: "mongodb",
       },
       { url: "https://github.com/tlomiej", name: "Github", icon: "uil:github" },
     ],
