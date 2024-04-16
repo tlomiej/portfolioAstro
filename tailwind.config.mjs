079f341bad93448c45f0799d7be1f0398a56d7bb
tlomiej <bartekstaron@gmail.com>
2024-04-16 12:55:41 +0200

    astrolink

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,26 @@ export default {
       colors: {
         astroMainColor: "#827191",
         astroMainTextColor: "white",
-		astroLinkIconColor: '#827191',// gray-400
-		astroDemoLinkColor: "#7D1D3F"
+        astroLinkIconColor: "#827191", // gray-400
+        astroDemoLinkColor: "#7D1D3F",
+      },
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate(0.0deg)" },
+          "100%": { transform: "rotate(0.0deg)" },
+        },
+      },
+      animation: {
+        "waving-demo": "wave 1s linear",
       },
     },
   },
+
   plugins: [],
 };
