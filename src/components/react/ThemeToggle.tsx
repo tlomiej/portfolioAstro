77c7d89098320d15b7cbeba5dec1d4b818380c86
tlomiej <bartekstaron@gmail.com>
2024-04-23 09:03:28 +0200

    dark mode icons

--- src/components/react/ThemeToggle.tsx
+++ src/components/react/ThemeToggle.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
-    const initialTheme = typeof window !== 'undefined' ? localStorage.getItem("theme") ?? "light" : "light";
-    const [theme, setTheme] = useState(initialTheme);
-  
+  const initialTheme =
+    typeof window !== "undefined"
+      ? localStorage.getItem("theme") ?? "light"
+      : "light";
+
+  // Use the initialTheme as the initial state
+  const [theme, setTheme] = useState(initialTheme);
 
   const handleClick = () => {
     setTheme(theme === "light" ? "dark" : "light");
@@ -19,6 +23,38 @@ export default function ThemeToggle() {
   }, [theme]);
 
   return (
-    <button onClick={handleClick}>{theme === "light" ? "🌙" : "🌞"}</button>
+    <button
+      title="Change display mode"
+      className=" text-astroMainColor border-2 border-astroMainColor rounded-lg p-2 dark:border-astroDarkMainColor dark:text-astroDarkMainColor"
+      onClick={handleClick}
+    >
+      {theme === "dark" ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+          className="w-6 h-6"
+        >
+          <path
+            fill="currentColor"
+            d="M11 5V1h2v4zm6.65 2.75l-1.375-1.375l2.8-2.875l1.4 1.425zM19 13v-2h4v2zm-8 10v-4h2v4zM6.35 7.7L3.5 4.925l1.425-1.4L7.75 6.35zm12.7 12.8l-2.775-2.875l1.35-1.35l2.85 2.75zM1 13v-2h4v2zm3.925 7.5l-1.4-1.425l2.8-2.8l.725.675l.725.7zM12 18q-2.5 0-4.25-1.75T6 12t1.75-4.25T12 6t4.25 1.75T18 12t-1.75 4.25T12 18"
+          ></path>
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          width="1em"
+          viewBox="0 0 24 24"
+          className="w-6 h-6"
+        >
+          <path
+            fill="currentColor"
+            d="M6 14q1.2 0 2.2.65t1.475 1.775l.25.575h.625q1.05 0 1.75.738T13 19.5q0 1.05-.725 1.775T10.5 22H6q-1.65 0-2.825-1.175T2 18q0-1.675 1.175-2.838T6 14m5.25-11q-.45 2.475.275 4.838t2.5 4.137t4.138 2.5T23 14.75q-.65 3.55-3.375 5.863T13.325 23q.8-.65 1.238-1.562T15 19.5q0-1.7-1.062-2.937t-2.713-1.488q-.8-1.425-2.187-2.25T6 12q-.8 0-1.562.2T3 12.8q.05-3.625 2.363-6.375T11.25 3"
+          ></path>
+        </svg>
+      )}
+    </button>
   );
 }
