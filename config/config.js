16815ddd57592d7608aaa451b4b0c55741cdf7a9
tlomiej <bartekstaron@gmail.com>
2024-12-16 21:16:19 +0100

    config

--- config/config.js
+++ config/config.js
@@ -145,6 +145,7 @@ export const projectsbyID = {
       "Brawl Stars is a popular mobile game with millions of users around the world. The application is built upon the public Brawl Stars API, which provides convenient access to browse data and retrieve it in JSON format. With this API, users can effortlessly explore various game-related information and retrieve it in a structured JSON format for further processing or integration with other systems.",
     href: "/homeprojects/brawlstarapi",
     link: "https://learn-nodejs-r8oc.onrender.com",
+    repoLink: undefined,
     tools: [
       {
         url: "https://nodejs.org",
@@ -172,6 +173,7 @@ export const projectsbyID = {
   streamlit: {
     name: "Data visualization - Streamlit",
     link: "https://choropleth-gus-maps.streamlit.app/",
+    repoLink: undefined,
     href: "/homeprojects/streamlit",
     descryption:
       "Welcome to Voivodeship Data Visualizer, an intuitive web application built with Streamlit that brings you comprehensive insights into Poland's voivodeships through dynamic choropleth maps. Powered by data from the Central Statistical Office (GUS), this tool provides a visually engaging way to explore regional statistics. Key features include: Interactive Choropleth Maps: Seamlessly visualize key statistics for each voivodeship with interactive maps that highlight regional differences in various data categories. Up-to-date Data: Access the latest data from GUS, ensuring your insights are based on current and accurate information. User-friendly Interface: Enjoy a clean and responsive design that makes data exploration easy and engaging for users of all backgrounds.Detailed Data Insights: Drill down into specific voivodeships to view detailed statistical data, making it easier to understand regional trends and patterns. Customizable Views: Tailor your data visualization experience with customizable filters and options to focus on the information that matters most to you. Discover the power of data-driven insights with Voivodeship Data Visualizer, where data meets clarity and precision!",
@@ -192,6 +194,7 @@ export const projectsbyID = {
   geosearch: {
     name: "GeoSearch",
     link: "https://geoapp-8zbw.onrender.com",
+    repoLink: undefined,
     href: "/homeprojects/geosearch",
     descryption:
       "This product facilitates searching for locations based on their name and space. Users can apply specific filters to refine their search results. It automatically logs the search history, allowing users to revisit previous searches.Its interface is designed to be intuitive, providing users with a seamless experience.",
@@ -217,6 +220,7 @@ export const projectsbyID = {
   esrimap: {
     name: "Map with ESRI",
     link: "https://esri-app.onrender.com",
+    repoLink: undefined,
     href: "homeprojects/esrimap",
     descryption: "Map created wid ESRI JS API 4.30",
     tools: [
@@ -238,8 +242,9 @@ export const projectsbyID = {
     ],
   },
   portfolio: {
-    name: "portfolio",
+    name: "Portfolio",
     link: "https://github.com/tlomiej/portfolioAstro",
+    repoLink: "https://github.com/tlomiej/portfolioAstro", 
     href: "homeprojects/portfolio",
     descryption:
       "Template portfolio with config and tailwind configuration. Simple configuration. In the config.js file, you configure the content of the page: information about yourself, project descriptions, technologies. In the tailwind.config.mjs file, you define your colors.",
@@ -254,6 +259,7 @@ export const projectsbyID = {
   collector: {
     name: "Collector",
     link: "https://collect.pythonanywhere.com",
+    repoLink: undefined,
     href: "/homeprojects/collector",
     descryption:
       "Collector Pro is an advanced application designed to simplify and enhance the process of creating custom forms, collecting data, and generating insightful reports. Ideal for businesses, educational institutions, non-profits, and any organization that relies on efficient data collection and analysis, Collector Pro offers a comprehensive suite of tools to meet and exceed your needs. Collector Pro makes data collection straightforward and efficient. Once your forms are created, you can distribute them through various channels",
