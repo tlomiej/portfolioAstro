6d2b0a939c752ac332c8fcbf86b63e6169c36097
tlomiej <bartekstaron@gmail.com>
2025-12-16 14:10:02 +0100

    fix: correct spelling of 'description' in multiple files

--- config/config.js
+++ config/config.js
@@ -75,7 +75,7 @@ export const outerProfilLinks = [
 export const footerText = "Copyright 2025. Staron";
 export const aboutMe = {
   title: "Developer",
-  descryption:
+  description:
     " Developer skilled in frontend and backend development, specializing in Geographic Information Systems. Creates user-friendly interfaces and robust backend systems to analyze and visualize spatial data efficiently.",
 };
 
@@ -142,7 +142,7 @@ export const tools = [
 export const projectsbyID = {
   brawlstarapi: {
     name: "BrawlStarAPI",
-    descryption:
+    description:
       "Brawl Stars is a popular mobile game with millions of users around the world. The application is built upon the public Brawl Stars API, which provides convenient access to browse data and retrieve it in JSON format. With this API, users can effortlessly explore various game-related information and retrieve it in a structured JSON format for further processing or integration with other systems.",
     href: "/homeprojects/brawlstarapi",
     link: "https://learn-nodejs-r8oc.onrender.com",
@@ -178,7 +178,7 @@ export const projectsbyID = {
     link: "https://choropleth-gus-maps.streamlit.app/",
     repoLink: undefined,
     images: [],
-    descryption:
+    description:
       "Welcome to Voivodeship Data Visualizer, an intuitive web application built with Streamlit that brings you comprehensive insights into Poland's voivodeships through dynamic choropleth maps. Powered by data from the Central Statistical Office (GUS), this tool provides a visually engaging way to explore regional statistics. Key features include: Interactive Choropleth Maps: Seamlessly visualize key statistics for each voivodeship with interactive maps that highlight regional differences in various data categories. Up-to-date Data: Access the latest data from GUS, ensuring your insights are based on current and accurate information. User-friendly Interface: Enjoy a clean and responsive design that makes data exploration easy and engaging for users of all backgrounds.Detailed Data Insights: Drill down into specific voivodeships to view detailed statistical data, making it easier to understand regional trends and patterns. Customizable Views: Tailor your data visualization experience with customizable filters and options to focus on the information that matters most to you. Discover the power of data-driven insights with Voivodeship Data Visualizer, where data meets clarity and precision!",
     tools: [
       {
@@ -200,7 +200,7 @@ export const projectsbyID = {
     link: "https://geoapp-8zbw.onrender.com",
     repoLink: undefined,
     images: [],
-    descryption:
+    description:
       "This product facilitates searching for locations based on their name and space. Users can apply specific filters to refine their search results. It automatically logs the search history, allowing users to revisit previous searches.Its interface is designed to be intuitive, providing users with a seamless experience.",
     tools: [
       {
@@ -227,7 +227,7 @@ export const projectsbyID = {
     link: "https://esri-app.onrender.com",
     repoLink: undefined,
     images: [],
-    descryption: `OSM Search - Interactive map application for searching OpenStreetMap data with ESRI JS API.`,
+    description: `OSM Search - Interactive map application for searching OpenStreetMap data with ESRI JS API.`,
     tools: [
       {
         url: "https://react.dev",
@@ -252,7 +252,7 @@ export const projectsbyID = {
     link: "https://www.osmdata.online/",
     repoLink: undefined,
     images: ["/projects/osmdata/OSMSearch1.jpg"],
-    descryption: `OSM data search engine via the Overpass API service. 
+    description: `OSM data search engine via the Overpass API service. 
     
     
     It offers both attribute and spatial filters for efficient retrieval of the required data.
@@ -287,7 +287,7 @@ export const projectsbyID = {
     link: "https://github.com/tlomiej/portfolioAstro",
     repoLink: "https://github.com/tlomiej/portfolioAstro",
     images: [],
-    descryption:
+    description:
       "Template portfolio with config and tailwind configuration. Simple configuration. In the config.js file, you configure the content of the page: information about yourself, project descriptions, technologies. In the tailwind.config.mjs file, you define your colors.",
     tools: [
       {
@@ -303,7 +303,7 @@ export const projectsbyID = {
     repoLink: undefined,
     href: "/homeprojects/collector",
     images: [],
-    descryption:
+    description:
       "Collector Pro is an advanced application designed to simplify and enhance the process of creating custom forms, collecting data, and generating insightful reports. Ideal for businesses, educational institutions, non-profits, and any organization that relies on efficient data collection and analysis, Collector Pro offers a comprehensive suite of tools to meet and exceed your needs. Collector Pro makes data collection straightforward and efficient. Once your forms are created, you can distribute them through various channels",
     tools: [
       {
